c243d9b0fd887dccd058f9c1af8226b9f4651b9a
Sjors Jessen <sjors.jessen@gmail.com>
2021-03-26 13:40:27 +0100

    Made Classes, implemented states and component life circles

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,127 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Confirm from "./Confirm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and TypeScript
-        </a>
-      </header>
-    </div>
-  );
+interface IState{
+    confirmDialogOpen: boolean;
+    confirmMessage: string;
+    confirmVisible:boolean;
+    countDown:number;
+}
+
+class App extends React.Component<{}, IState> {
+    private timer: number = 0;
+    private renderCount: number = 0;
+    
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            confirmMessage: "Please, click confirm",
+            confirmDialogOpen: false,
+            confirmVisible: true,
+            countDown: 10,
+        };
+    }
+
+    public static getDerivedStateFromProps(props: {}, state: IState) {
+        console.log("getDerivedStateFromProps", props, state);
+        return null;
+    }
+
+    public shouldComponentUpdate(nextProps: {}, nextState: IState)
+    {
+        console.log("shouldComponentUpdate", nextProps, nextState);
+        return true;
+    }
+    
+    public getSnapshotBeforeUpdate(prevProps: {}, prevState: IState) {
+        this.renderCount += 1;
+        console.log("getSnapshotBeforeUpdate", prevProps, prevState, {renderCount: this.renderCount});
+        return this.renderCount;
+    }
+    
+    public componentDidUpdate(prevProps: {}, prevState: IState, snapshot: number) {
+        console.log("componentDidUpdate", prevProps, prevState, snapshot, {renderCount: this.renderCount});
+    }
+    
+    public componentDidMount() {
+        this.timer = window.setInterval(() => this.handleTimerClick(), 1000)
+    }
+
+    public componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+    
+    private handleOpenDialogClick = () => {
+        this.setState({ confirmDialogOpen: true });
+        clearInterval(this.timer);
+    }
+    
+    private handleCancelClick = () => {
+        this.setState({ 
+            confirmMessage: "Take a break, I'm sure you will later", 
+            confirmDialogOpen: false 
+        });
+        clearInterval(this.timer);
+    };
+    
+    private handleConfirmClick = () => {
+        this.setState({ 
+            confirmMessage: "Cool, carry on reading!", 
+            confirmDialogOpen: false 
+        });
+        clearInterval(this.timer);
+    };
+
+    private handleTimerClick() {
+        this.setState(
+            {
+                confirmMessage: `Please hit the confirm button ${this.state.countDown} secs to go`,
+                countDown: this.state.countDown - 1
+            },
+            () => {
+                if (this.state.countDown <= 0) {
+                    clearInterval(this.timer);
+                    this.setState({
+                        confirmMessage: "Too late to confirm!",
+                        confirmVisible: false
+                    });
+                }
+            }
+        );
+    }
+    
+  public render (){
+      return(
+          <div className="App">
+              <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                      Edit <code>src/App.tsx</code> and save to reload.
+                  </p>
+                  <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                      Learn React and TypeScript
+                  </a>
+              </header>
+              <p>{this.state.confirmMessage}</p>
+              {this.state.confirmVisible && (<button onClick={this.handleOpenDialogClick}>Confirm</button>)}
+              <Confirm 
+                  open={this.state.confirmDialogOpen}
+                  title="This is where our title should go" 
+                  content="This is where our content should go" 
+                  confirmCaption="Yes, please!"
+                  onConfirmClick={this.handleConfirmClick}
+                  onCancelClick={this.handleCancelClick}
+              />
+          </div>
+        )
+    }
 }
 
 export default App;
---
+++ my-app/src/Confirm.tsx
@@ -0,0 +1,48 @@
+﻿import React from "react";
+import "./Confirm.css"
+
+interface IProps{
+    open:boolean,
+    title:string;
+    content:string;
+    cancelCaption?:string;
+    confirmCaption?:string;
+    onConfirmClick: () => void;
+    onCancelClick: () => void;
+}
+
+class Confirm extends React.Component<IProps>{
+    public static defaultProps = {
+        cancelCaption: "Cancel",
+        confirmCaption: "Confirm"
+    }
+    
+    public render() {
+        return (
+            <div className={this.props.open ? "confirm-wrapper confirm-visible" : "confirm-wrapper"}>
+                <div className="confirm-container">
+                    <div className="confirm-title-container">
+                        <span>{this.props.title}</span>
+                    </div>
+                    <div className="confirm-content-container">
+                        <p>{this.props.content}</p>
+                    </div>
+                    <div className="confirm-buttons-container">
+                        <button onClick={this.handleCancelClick} className="confirm-cancel">{this.props.cancelCaption}</button>
+                        <button onClick={this.handleConfirmClick} className="confirm-ok">{this.props.confirmCaption}</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    private handleConfirmClick = () => {
+        this.props.onConfirmClick();
+    }
+
+    private handleCancelClick = () => {
+        this.props.onCancelClick();
+    }
+}
+
+export default Confirm;
