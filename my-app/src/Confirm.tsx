802368279b3644b8ad26d813a26f5ab0e6203430
Sjors Jessen <sjors.jessen@gmail.com>
2021-03-26 14:29:25 +0100

    Converted class to FunctionalComponent

--- my-app/src/Confirm.tsx
+++ my-app/src/Confirm.tsx
@@ -11,38 +11,37 @@ interface IProps{
     onCancelClick: () => void;
 }
 
-class Confirm extends React.Component<IProps>{
-    public static defaultProps = {
-        cancelCaption: "Cancel",
-        confirmCaption: "Confirm"
+const Confirm: React.FC<IProps> = (props) =>{
+    
+    const handleConfirmClick = () => {
+        props.onConfirmClick();
+    }
+
+    const handleCancelClick = () => {
+        props.onCancelClick();
     }
     
-    public render() {
-        return (
-            <div className={this.props.open ? "confirm-wrapper confirm-visible" : "confirm-wrapper"}>
-                <div className="confirm-container">
-                    <div className="confirm-title-container">
-                        <span>{this.props.title}</span>
-                    </div>
-                    <div className="confirm-content-container">
-                        <p>{this.props.content}</p>
-                    </div>
-                    <div className="confirm-buttons-container">
-                        <button onClick={this.handleCancelClick} className="confirm-cancel">{this.props.cancelCaption}</button>
-                        <button onClick={this.handleConfirmClick} className="confirm-ok">{this.props.confirmCaption}</button>
-                    </div>
+    return (
+        <div className={props.open ? "confirm-wrapper confirm-visible" : "confirm-wrapper"}>
+            <div className="confirm-container">
+                <div className="confirm-title-container">
+                    <span>{props.title}</span>
+                </div>
+                <div className="confirm-content-container">
+                    <p>{props.content}</p>
+                </div>
+                <div className="confirm-buttons-container">
+                    <button onClick={handleCancelClick} className="confirm-cancel">{props.cancelCaption}</button>
+                    <button onClick={handleConfirmClick} className="confirm-ok">{props.confirmCaption}</button>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+}
 
-    private handleConfirmClick = () => {
-        this.props.onConfirmClick();
-    }
-
-    private handleCancelClick = () => {
-        this.props.onCancelClick();
-    }
+Confirm.defaultProps = {
+    cancelCaption: "Cancel",
+    confirmCaption: "Confirm"
 }
 
 export default Confirm;
