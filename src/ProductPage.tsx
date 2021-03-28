89520f2b5c04a5e017c3522c4c3ef499e51669ef
Sjors Jessen <sjors.jessen@gmail.com>
2021-03-28 21:18:29 +0200

    Route Prompt

--- src/ProductPage.tsx
+++ src/ProductPage.tsx
@@ -1,5 +1,5 @@
 ﻿import * as React from "react";
-import { RouteComponentProps } from "react-router-dom";
+import { RouteComponentProps, Prompt } from "react-router-dom";
 import { IProduct, products } from "./ProductsData";
 
 type Props = RouteComponentProps<{id: string}>;
@@ -28,12 +28,15 @@ class ProductPage extends React.Component<Props, IState> {
     private handleAddClick = () => {
         this.setState({ added: true });
     };
+
+    private navAwayMessage = () => "Are you sure you leave without buying this product?";
     
     public render() {
         const product = this.state.product;
         
         return (
             <div className="page-container">
+                <Prompt when={!this.state.added} message={this.navAwayMessage}/>
                 {product ? 
                 (
                     <React.Fragment>
