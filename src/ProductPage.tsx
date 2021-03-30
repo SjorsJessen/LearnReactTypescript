87efcce76d0f8106056ecb416faa4e66cdbb6131
Sjors Jessen <sjors.jessen@gmail.com>
2021-03-30 21:48:48 +0200

    Container and presentational components

--- src/ProductPage.tsx
+++ src/ProductPage.tsx
@@ -1,6 +1,7 @@
 ﻿import * as React from "react";
 import { RouteComponentProps, Prompt } from "react-router-dom";
 import { IProduct, products } from "./ProductsData";
+import Product from "./Product";
 
 type Props = RouteComponentProps<{id: string}>;
 
@@ -39,19 +40,11 @@ class ProductPage extends React.Component<Props, IState> {
                 <Prompt when={!this.state.added} message={this.navAwayMessage}/>
                 {product ? 
                 (
-                    <React.Fragment>
-                        <h1>{product.name}</h1>
-                        <p>{product.description}</p>
-                        <p className="product-price">
-                            {new Intl.NumberFormat("en-US", {
-                                currency: "USD",
-                                style: "currency"
-                            }).format(product.price)}
-                        </p>
-                        {!this.state.added && (
-                            <button onClick={this.handleAddClick}>Add to basket</button>
-                        )}
-                    </React.Fragment>
+                    <Product
+                        product={product}
+                        inBasket={this.state.added}
+                        onAddToBasket={this.handleAddClick}
+                    />
                 ) : (<p>Product not found!</p>)}
             </div>
         );
