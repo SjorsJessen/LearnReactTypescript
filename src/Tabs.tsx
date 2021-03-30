7c7884ce8dd04d1e864ac85369330e9e0cfc8abb
Sjors Jessen <sjors.jessen@gmail.com>
2021-03-30 22:38:36 +0200

    Sharing state with React Context

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,38 +1,53 @@
 ﻿import * as React from "react";
 
-interface IProps {
-    headings: string[];
+interface ITabsContext {
+    activeName?: string;
+    handleTabClick?: (name: string) => void;
 }
+const TabsContext = React.createContext<ITabsContext>({});
 
 interface IState {
-    activeHeading: string;
+    activeName: string;
 }
 
-class Tabs extends React.Component<IProps, IState> {
-    public constructor(props: IProps) {
-        super(props);
-        this.state = {
-            activeHeading: this.props.headings && this.props.headings.length > 0 ? this.props.headings[0] : ""
-        };
-    }
+class Tabs extends React.Component<{ }, IState> {
+    public static Tab: React.FC<ITabProps> = props => (
+        <TabsContext.Consumer>
+            {(context: ITabsContext) => {
+                const activeName = context.activeName ? context.activeName : props.initialActive ? props.name : "";
+                const handleTabClick = (e: React.MouseEvent<HTMLLIElement>) =>
+                {
+                    if (context.handleTabClick) {
+                        context.handleTabClick(props.name);
+                    }
+                };
+                return (
+                    <li onClick={handleTabClick} className={props.name === activeName ? "active" : ""}>
+                        {props.children}
+                    </li>
+                );
+            }}
+        </TabsContext.Consumer>
+    );
 
-    private handleTabClick = (e: React.MouseEvent<HTMLLIElement>) => {
-        const li = e.target as HTMLLIElement;
-        const heading: string = li.textContent ? li.textContent : "";
-        this.setState({ activeHeading: heading });
+    private handleTabClick = (name: string) => {
+        this.setState({ activeName: name });
     };
     
     public render() {
         return (
-            <ul className="tabs">
-                {this.props.headings.map(heading => (
-                    <li onClick={this.handleTabClick} className={heading === this.state.activeHeading ? "active" : ""}>
-                        {heading}
-                    </li>
-                ))}
-            </ul>
+            <TabsContext.Provider 
+                value={{activeName: this.state ? this.state.activeName : "", handleTabClick: this.handleTabClick}}
+            >
+                <ul className="tabs">{this.props.children}</ul>
+            </TabsContext.Provider>
         );
     }
 }
 
 export default Tabs;
+
+interface ITabProps {
+    name: string;
+    initialActive?: boolean;
+}
