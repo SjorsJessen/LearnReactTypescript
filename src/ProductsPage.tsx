dceacd702d2c115d60f097278d58474ce05322a3
SjorsJessen <sjors.jessen@gmail.com>
2021-04-05 20:39:47 +0200

    Connecting ProductsPage to the loading store state

--- src/ProductsPage.tsx
+++ src/ProductsPage.tsx
@@ -1,11 +1,12 @@
 ﻿import React from "react";
 import { IProduct } from "./ProductsData";
-import { Link, RouteComponentProps } from "react-router-dom";
+import { RouteComponentProps } from "react-router-dom";
 import "url-search-params-polyfill";
 
 import { connect } from "react-redux";
 import { IApplicationState } from "./Store";
 import { getProducts } from "./ProductsActions";
+import ProductsList from "./ProductsList";
 interface IProps extends RouteComponentProps {
     getProducts: typeof getProducts;
     loading: boolean;
@@ -18,25 +19,17 @@ class ProductsPage extends React.Component<IProps>{
     }
 
     public render() {
-        const searchParams = new
-            URLSearchParams(this.props.location.search);
+        const searchParams = new URLSearchParams(this.props.location.search);
         const search = searchParams.get("search") || "";
+
         return (
             <div className="page-container">
                 <p>Welcome to React Shop where you can get all your tools for ReactJS!</p>
-                <ul className="product-list">
-                    {this.props.products.map(product => {
-                        if (!search || (search &&
-                            product.name.toLowerCase().indexOf(search.toLowerCase()) > -1)
-                        ) {
-                            return (
-                                <li key={product.id} className="product-list-item">
-                                    <Link to={`/products/${product.id}`}>{product.name}</Link>
-                                </li>
-                            );
-                        } else { return null; }
-                    })}
-                </ul>
+                <ProductsList
+                    search={search}
+                    products={this.props.products}
+                    loading={this.props.loading}
+                />
             </div>
         );
     }
