f4eb7d8026e73f892fc3e19340bcb08c5a63ea67
SjorsJessen <sjors.jessen@gmail.com>
2021-04-05 20:12:12 +0200

    Added Provider and connected ProductsPage

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,28 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Routes from "./Routes";
 
+import { Provider } from "react-redux";
+import { Store } from 'redux';
+import configureStore from "./Store";
+import { IApplicationState } from "./Store";
+
+interface IProps {
+  store: Store<IApplicationState>;
+}
+
+const Root: React.FC<IProps> = props => {
+  return (
+    <Provider store={props.store}>
+      <Routes />
+    </Provider>
+  );
+};
+
+const store = configureStore();
+
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <Root store={store} />,
   </React.StrictMode>,
   document.getElementById('root')
 );
